48025441f077a04abd6fd4611004741e55c8e2d4
Janelle Kuhns <j3ckuhns@gmail.com>
2020-05-19 20:54:57 -0700

    added fields to model

--- customuserapp/models.py
+++ customuserapp/models.py
@@ -4,4 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class MyUser(AbstractUser):
-	display_name = models.CharField(max_length=50, null=True, blank=True)
+	display_name = models.CharField(max_length=50)
+	username = models.CharField(max_length=50)
+	age = models.IntegerField(default=0)
