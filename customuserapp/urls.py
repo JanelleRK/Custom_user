722cd4797747dae43b95e8cc7149bbb3529766ec
Janelle Kuhns <j3ckuhns@gmail.com>
2020-06-03 07:25:24 -0700

    adding login_view

--- customuserapp/urls.py
+++ customuserapp/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from customuserapp.views import index, sign_up_view, logout_view
+from customuserapp import views
 
 urlpatterns = [
-    path('', index, name='homepage'),
-	path('signup/', sign_up_view),
-	path('logout/', logout_view)
+    path('', views.index, name='homepage'),
+	path('signup/', views.sign_up_view, name='signup'),
+	path('login/', views.login_view, name='login'),
+	path('logout/', views.logout_view, name='logout')
 ]
